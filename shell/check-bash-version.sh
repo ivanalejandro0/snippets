4ca150075e89026b37fb38cf8273025f6b408aae
Ivan Alejandro <ivanalejandro0@gmail.com>
2019-10-04 12:03:32 -0300

    Check version using the BASH_VERSINFO array

--- shell/check-bash-version.sh
+++ shell/check-bash-version.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # bash v4 introduces many features, like associative arrays
-MIN_VERSION=4
+REQUIRED_MAJOR_VERSION=4
 
 # Multi line with message
-if (( ${BASH_VERSION%%.*} < MIN_VERSION )); then
-    echo "This script needs bash >= $MIN_VERSION"
+if (( ${BASH_VERSINFO[0]} < REQUIRED_MAJOR_VERSION )); then
+    echo "This script needs bash >= $REQUIRED_MAJOR_VERSION"
     exit 1
 fi
 
